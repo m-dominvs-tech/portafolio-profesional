0fb4f2be15a442fe601a4ae6c86ff22faab36592
Matias Dominvs <matias.morales.olave@gmail.com>
2026-04-15 15:36:54 -0400

    Style:  mordenizando el formulario de contacto

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-const conectarDB = async () =>{
-    try{
-           await mongoose.connect(process.env.MONGO_URI);
-           console.log("'🚀 Base de datos conectada para el proyecto Portafolio")
-    }catch(error){
-        console.error('❌ Error al conectar:', error);
+const conectarDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("'🚀 Base de datos conectada para el proyecto Portafolio");
+  } catch (error) {
+    console.error("❌ Error al conectar:", error);
     process.exit(1); // Detener si falla
-    }
+  }
 };
 
 module.exports = conectarDB;
